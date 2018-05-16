531ace20c531a88d1ee13ac14380ca8394fdbf9f
Lucas Pena <lpena7@illinois.edu>
2018-05-16 14:44:02 -0500

    fo-lfp: Wrote skeletons for functions we will need for unfolding

---
+++ fo-lfp.py
@@ -0,0 +1,36 @@
+from z3 import *
+
+# Check if pre /\ code /\ ~post is satisfiable. Return model or None if unsat
+def main(pre, code, post):
+    neg_post = Not(post)
+    s = Solver()
+    s.add(pre, code, neg_post)
+    is_sat = s.check()
+    print is_sat
+    if (is_sat == sat):
+        return s.model()
+    return None
+
+# Test
+def test():
+    f = Function('f', IntSort(), IntSort())
+    a = Int('a')
+    b = Int('b')
+    pre = a + b > 5
+    code = b < 1
+    post = a == 6
+    return main(pre, code, post)
+
+# Instantiate all rec_defs with all terms found so far
+def instantiate(terms, rec_defs):
+    return None
+
+# Collect ground terms from formulas
+def collect_terms(formulas):
+    return None
+
+# Parse input file
+def parse_file(input_file):
+    return None
+
+print test()
