eb2a597a461731b39d477eb5e6d9f47162062384
Lucas Pena <lpena7@illinois.edu>
2018-05-23 15:07:31 -0500

    updated toy example with rec def where instantiation is needed to prove unsat

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
+from z3 import *
 from fo_lfp import check_sat
 
 # Test
 def test():
+    Iff = lambda f: And(Implies(f[0],f[1]),Implies(f[1],f[0]))
+
     a = Int('a')
     b = Int('b')
-    pre = a + b > 5
-    code = b < 1
-    post = a == 6
-    return check_sat(pre, code, post)
+    x = Int('x')
+
+    nextf = Function('next', IntSort(), IntSort())
+
+    listf = Function('list', IntSort(), BoolSort())
+    rho_list = Iff((listf(x), Or(x == -1, And(x != -1, listf(nextf(x))))))
+
+    pre = True
+    code = a == -1
+    post = listf(a)
+
+    # Without this instantiation, check_sat returns satisfiable
+    inst = substitute(rho_list, (x, a))
+
+    return check_sat([pre, code, inst], post)
+
+print test()
