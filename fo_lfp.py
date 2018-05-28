53f2bc2ecf533e18621792486e4d906f08d53271
Lucas Pena <lpena7@illinois.edu>
2018-05-28 18:02:37 -0500

    Finished toy example for instantiating
    Removed dependency on z3py and json. Formulas represented as a list of lists to avoid any parsing issues

--- fo_lfp.py
+++ fo_lfp.py
@@ -1,6 +1,3 @@
-from z3 import *
-import json
-
 # Check if formulas added to solver are satisfiable. Return model or None if unsat
 def check_sat(solver):
     is_sat = solver.check()
@@ -10,34 +7,90 @@ def check_sat(solver):
         return solver.model()
     return None
 
+# Replace var in rec def with term
+def replace(in_list, var, term):
+    if in_list == []:
+        return []
+
+    if in_list[0] == [var]:
+        return [term] + replace(in_list[1:], var, term)
+
+    if type(in_list[0]) != list:
+        return [in_list[0]] + replace(in_list[1:], var, term)
+
+    if type(in_list[0]) == list:
+        return [replace(in_list[0], var, term)] + replace(in_list[1:], var, term)
+
+# Instantiate one rec_def with all terms found so far
+def instantiate_one(terms, variables, rec_def):
+    if variables == []:
+        return [rec_def]
+
+    partial_rec_defs = []
+    for term in terms:
+        partial_rec_defs += replace(rec_def, variables[0], term)
+    
+    return instantiate_many(terms, variables[1:], partial_rec_defs) 
+
 # Instantiate all rec_defs with all terms found so far
-def instantiate(pre, code, post, replacements, rec_defs):
-    neg_post = Not(post)
-    s = Solver()
-    s.add(pre)
-    s.add(code)
-    s.add(neg_post)
+def instantiate_many(terms, variables, rec_defs):
+    inst = []
+    for rec_def in rec_defs:
+        inst += instantiate_one(terms, variables, rec_def)
 
-    for replacement in replacements:
-        for rec_def in rec_defs:
-            s.add(substitute(rec_def, replacement))
+    return inst
 
-    return check_sat(s)
+# Helper for instantiating
+# TODO: make sure correct with many variables
+def instantiate(terms, variables, rec_defs):
+    out = []
+    for term in terms:
+        out += [instantiate_many([term], variables, rec_defs)]
+    return out
+
+# Collect ground terms from formula
+def collect_terms_formula(formula):
+    constants = ["True", "False"]
+    formulas = ["not", "and", "or", "iff", "==", "!="]
+    rec_defs = ["list"]
+    unwanted = constants + formulas + rec_defs
+    
+    terms = ["next"]
+    all_connectors = unwanted + terms
+
+    if formula == []:
+        return []
+
+    elif formula[0] in unwanted:
+        return collect_terms_formulas(formula[1:])
+
+    # TODO: may not need this case
+    elif not(formula[0].lstrip("-").isdigit()) and formula[0] not in all_connectors:
+        # TODO: make sure there is nothing else in the list
+        return [[formula[0]]]
+
+    elif formula[0] in terms:
+        return [formula] + collect_terms_formulas(formula[1:])
+    
+    else:
+        # throw error correctly
+        print "ERROR"
 
 # Collect ground terms from formulas
-def collect_terms(formulas):
-    return None
+def collect_terms_formulas(formulas):
 
-# Parse input file
-# TODO: need to be able to initialize variable number of definitions
-# TODO: we probably don't need this method and can have inputs from an external python file
-def parse_file(input_file):
-    in_file = json.load(open(input_file))
-    a = eval(in_file["defs"][0])
-    b = eval(in_file["defs"][1])
-    pre = eval(in_file["pre"])
-    code = eval(in_file["code"])
-    post = eval(in_file["post"])
-    return check_sat(pre, code, post)
+    out = []
 
-# print parse_file("test.json")
+    for formula in formulas:
+        out += collect_terms_formula(formula)
+
+    return remove_duplicates(out)
+
+# TODO: remove duplicate items in list of terms
+def remove_duplicates(init_list):
+    final_list = []
+    for item in init_list:
+        if item not in final_list:
+            final_list += [item]
+    return final_list
+
--- test.py
+++ test.py
@@ -1,29 +1,19 @@
 from z3 import *
-from fo_lfp import instantiate
+from fo_lfp import *
 
 # Test
 def test():
-    Iff = lambda f: And(Implies(f[0],f[1]),Implies(f[1],f[0]))
+    listf = ["iff", ["list", ["x"]],
+                    ["or", ["==", ["x"], ["nil"]],
+                           ["and", ["!=", ["x"], ["nil"]],
+                                   ["list", ["next", ["x"]]]]]]
 
-    a = Int('a')
-    b = Int('b')
-    x = Int('x')
+    pre = ["True"]
+    code = ["==", ["a"], ["nil"]]
+    post = ["list", ["next", ["a"]]]
 
-    nextf = Function('next', IntSort(), IntSort())
+    terms = collect_terms_formulas([pre, code, post])
 
-    listf = Function('list', IntSort(), BoolSort())
-    rho_list = Iff((listf(x), Or(x == -1, And(x != -1, listf(nextf(x))))))
-
-    pre = True
-    code = a == -1
-    post = listf(a)
-
-    # Uncomment out this and we see satisfiable
-    # return instantiate(pre, code, post, [], [rho_list])
-
-    # Instantiate x with a in all recursive definitions
-    return instantiate(pre, code, post, [(x, a)], [rho_list])
-
-    ## return check_sat([pre, code, inst], post)
+    return instantiate(terms, ["x"], [listf])
 
 print test()
