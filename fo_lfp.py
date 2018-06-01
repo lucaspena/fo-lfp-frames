fb13976d8fc6cbaae8910b4213c17eeed1098a3e
murali24adithya <murali24adithya@gmail.com>
2018-05-31 22:04:23 -0500

    Multi-variable instantiation "$" sign as the first element of the list skips the list as a term, 	only looks for subterms

--- fo_lfp.py
+++ fo_lfp.py
@@ -1,25 +1,37 @@
 # Check if formulas added to solver are satisfiable. Return model or None if unsat
-def check_sat(solver):
-    is_sat = solver.check()
-    print is_sat
-
-    if (is_sat == sat):
-        return solver.model()
-    return None
+#def check_sat(solver):
+#    is_sat = solver.check()
+#    print is_sat
+#
+#    if (is_sat == sat):
+#        return solver.model()
+#    return None
 
 # Replace var in rec def with term
-def replace(in_list, var, term):
-    if in_list == []:
-        return []
+#def replace(in_list, var, term):
+#    if in_list == []:
+#        return []
+#
+#    if in_list[0] == [var]:
+#        return [term] + replace(in_list[1:], var, term)
+#
+#    if type(in_list[0]) != list:
+#        return [in_list[0]] + replace(in_list[1:], var, term)
+#
+#    if type(in_list[0]) == list:
+#        return [replace(in_list[0], var, term)] + replace(in_list[1:], var, term)
 
-    if in_list[0] == [var]:
-        return [term] + replace(in_list[1:], var, term)
-
-    if type(in_list[0]) != list:
-        return [in_list[0]] + replace(in_list[1:], var, term)
-
-    if type(in_list[0]) == list:
-        return [replace(in_list[0], var, term)] + replace(in_list[1:], var, term)
+def replace_rec(in_list, var, term):
+    if type(in_list) !=list:
+        return in_list
+    elif in_list == [var]:
+        return term
+    else:
+        out = []
+        for elts in in_list:
+            out += [replace_rec(elts, var, term)] 
+            
+        return out
 
 # Instantiate one rec_def with all terms found so far
 def instantiate_one(terms, variables, rec_def):
@@ -28,63 +40,93 @@ def instantiate_one(terms, variables, rec_def):
 
     partial_rec_defs = []
     for term in terms:
-        partial_rec_defs += replace(rec_def, variables[0], term)
+        partial_rec_defs += [replace_rec(rec_def, variables[0], term)]
     
-    return instantiate_many(terms, variables[1:], partial_rec_defs) 
+    return instantiate(terms, variables[1:], partial_rec_defs) 
 
 # Instantiate all rec_defs with all terms found so far
-def instantiate_many(terms, variables, rec_defs):
+#def instantiate_many(terms, variables, rec_defs):
+def instantiate(terms, variables, rec_defs):
     inst = []
     for rec_def in rec_defs:
         inst += instantiate_one(terms, variables, rec_def)
 
     return inst
-
+            
 # Helper for instantiating
 # TODO: make sure correct with many variables
-def instantiate(terms, variables, rec_defs):
+#def instantiate(terms, variables, rec_defs):
+#    out = []
+#    for term in terms:
+#        out += [instantiate_many([term], variables, rec_defs)]
+#    return out
+
+
+def collect_terms(things):
+    constants = ["True", "False"]
+    bool_constructors = ["not", "and", "or", "iff", "==", "!="]
+    predicates = ["list"]
+    skip_symbols = ["$"]
+    skip_indicator = constants + bool_constructors + predicates + skip_symbols
+    
     out = []
-    for term in terms:
-        out += [instantiate_many([term], variables, rec_defs)]
+    if type(things) != list:
+        return []
+    
+    if len(things) == 0:
+        print("ERROR\n\n\n\n")
+    if things[0] not in skip_indicator:
+        out += [things]
+    if len(things) > 1:
+        for thing in things:
+            out += collect_terms(thing)
+    
+    return remove_duplicates(out)
+
+
+def collect_terms_formulas(formulas):
+    out = []
+    for formula in formulas:
+        out += collect_terms(formula)
+    
     return out
 
 # Collect ground terms from formula
-def collect_terms_formula(formula):
-    constants = ["True", "False"]
-    formulas = ["not", "and", "or", "iff", "==", "!="]
-    rec_defs = ["list"]
-    unwanted = constants + formulas + rec_defs
-    
-    terms = ["next"]
-    all_connectors = unwanted + terms
-
-    if formula == []:
-        return []
-
-    elif formula[0] in unwanted:
-        return collect_terms_formulas(formula[1:])
-
-    # TODO: may not need this case
-    elif not(formula[0].lstrip("-").isdigit()) and formula[0] not in all_connectors:
-        # TODO: make sure there is nothing else in the list
-        return [[formula[0]]]
-
-    elif formula[0] in terms:
-        return [formula] + collect_terms_formulas(formula[1:])
-    
-    else:
-        # throw error correctly
-        print "ERROR"
-
-# Collect ground terms from formulas
-def collect_terms_formulas(formulas):
-
-    out = []
-
-    for formula in formulas:
-        out += collect_terms_formula(formula)
-
-    return remove_duplicates(out)
+#def collect_terms_formula(formula):
+#    constants = ["True", "False"]
+#    formulas = ["not", "and", "or", "iff", "==", "!="]
+#    rec_defs = ["list"]
+#    unwanted = constants + formulas + rec_defs
+#    
+#    terms = ["next"]
+#    all_connectors = unwanted + terms
+#
+#    if formula == []:
+#        return []
+#
+#    elif formula[0] in unwanted:
+#        return collect_terms_formulas(formula[1:])
+#
+#    # TODO: may not need this case
+#    elif not(formula[0].lstrip("-").isdigit()) and formula[0] not in all_connectors:
+#        # TODO: make sure there is nothing else in the list
+#        return [[formula[0]]]
+#
+#    elif formula[0] in terms:
+#        return [formula] + collect_terms_formulas(formula[1:])
+#    
+#    else:
+#        # throw error correctly
+#        print("ERROR")
+#
+## Collect ground terms from formulas
+#def collect_terms_formulas(formulas):
+#    out = []
+#
+#    for formula in formulas:
+#        out += collect_terms_formula(formula)
+#
+#    return remove_duplicates(out)
 
 # TODO: remove duplicate items in list of terms
 def remove_duplicates(init_list):
--- test.py
+++ test.py
@@ -1,19 +1,33 @@
-from z3 import *
+#from z3 import *
 from fo_lfp import *
+from fo_lfp import remove_duplicates
+from fo_lfp import instantiate
+from fo_lfp import collect_terms_formulas
+
+
+def func():
+    print("Hello")
+
 
 # Test
 def test():
-    listf = ["iff", ["list", ["x"]],
-                    ["or", ["==", ["x"], ["nil"]],
-                           ["and", ["!=", ["x"], ["nil"]],
-                                   ["list", ["next", ["x"]]]]]]
+   listf = ["iff", ["list", ["x"]],
+                   ["or", ["==", ["x"], ["nil"]],
+                          ["and", ["!=", ["x"], ["nil"]],
+                                  ["list", ["next", ["x"]]]]]]
 
-    pre = ["True"]
-    code = ["==", ["a"], ["nil"]]
-    post = ["list", ["next", ["a"]]]
+   pre = ["True"]
+   code = ["==", ["a"], ["nil"]]
+   post = ["list", ["next", ["a"]]]
 
-    terms = collect_terms_formulas([pre, code, post])
+   terms = collect_terms_formulas([pre, code, post])
+#    listf = [["x"], ["y"], ["z"]]
+#    terms = [['a'], ['nil']]
+   return remove_duplicates(instantiate(terms, ["x", "y", "z"], [listf]))
 
-    return instantiate(terms, ["x"], [listf])
 
-print test()
+out = test()
+for elts in out:
+    print(elts)
+    print("\n")
+    
