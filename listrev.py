112de07c8c3348a574798b01d730e75f3a72b463
Lucas Pena <lpena7@illinois.edu>
2018-06-07 17:21:34 -0500

    listrev: cleaned up recursive definitions

--- listrev.py
+++ listrev.py
@@ -1,35 +1,39 @@
 from fo_lfp import *
 
+# list, keys, hlist recursive definitions
 Rlist = ["iff", ["list", ["x"]],
                 ["or", ["==", ["x"], ["nil"]],
                        ["and", ["!=", ["x"], ["nil"]],
                                ["list", ["next", ["x"]]]]]]
 
+Rhlist = ["==", ["hlist", ["x"]],
+                ["ite", ["==", ["x"], ["nil"]],
+                        ["\empty"],
+                        ["union", ["singleton", ["x"]],
+                                  ["hlist", ["next", ["x"]]]]]]
+
 Rkeys = ["==", ["keys", ["x"]],
                ["ite", ["==", ["x"], ["nil"]], 
-                       ["\empty"], 
+                       ["\empty"],
                        ["union", ["singleton", ["key", ["x"]]],
                                  ["keys", ["next", ["x"]]]]]]
 
-Rhlist = ["==", ["hlist", ["x"]],
-               ["ite", ["==", ["x"], ["nil"]], 
-                       ["\empty"], 
-                       ["union", ["singleton", ["x"]],
-                                 ["hlist", ["next", ["x"]]]]]]
-
+# primed versions of list, keys, hlist
 Rlist2 = ["iff", ["list2", ["x"]],
-                ["or", ["==", ["x"], ["nil"]],
-                       ["and", ["!=", ["x"], ["nil"]],
-                               ["list2", ["next2", ["x"]]]]]]
-
-Rkeys2 = ["==", ["keys2", ["x"]],
-               ["ite", ["==", ["x"], ["nil"]], 
-                       ["\empty"], 
-                       ["union", ["singleton", ["key", ["x"]]],
-                                 ["keys2", ["next2", ["x"]]]]]]
+                 ["or", ["==", ["x"], ["nil"]],
+                        ["and", ["!=", ["x"], ["nil"]],
+                                ["list2", ["next2", ["x"]]]]]]
 
 Rhlist2 = ["==", ["hlist2", ["x"]],
-               ["ite", ["==", ["x"], ["nil"]], 
-                       ["\empty"], 
-                       ["union", ["singleton", ["x"]],
-                                 ["hlist2", ["next2", ["x"]]]]]]
+                 ["ite", ["==", ["x"], ["nil"]],
+                         ["\empty"],
+                         ["union", ["singleton", ["x"]],
+                                   ["hlist2", ["next2", ["x"]]]]]]
+
+Rkeys2 = ["==", ["keys2", ["x"]],
+                ["ite", ["==", ["x"], ["nil"]],
+                        ["\empty"],
+                        ["union", ["singleton", ["key", ["x"]]],
+                                  ["keys2", ["next2", ["x"]]]]]]
+
+rec_defs = [Rlist, Rkeys, Rhlist, Rlist2, Rkeys2, Rhlist2]
