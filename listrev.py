b689f9c85af2e9989174c4da76bf3389c2803195
Lucas Pena <lpena7@illinois.edu>
2018-06-07 17:24:32 -0500

    listrev: define loop vc and do 2 levels of instantiation for listrev

--- listrev.py
+++ listrev.py
@@ -37,3 +37,43 @@ Rkeys2 = ["==", ["keys2", ["x"]],
                                   ["keys2", ["next2", ["x"]]]]]]
 
 rec_defs = [Rlist, Rkeys, Rhlist, Rlist2, Rkeys2, Rhlist2]
+
+## LOOP INVARIANT VC
+
+# precondition
+pre = ["and", ["list", ["orig"]],
+              ["list", ["rev"]],
+              ["==", ["intersect", ["hlist", ["orig"]], ["hlist", ["rev"]]], ["\empty"]],
+              ["==", ["union", ["keys", ["orig"]], ["keys", ["rev"]]], ["$", "kk"]]]
+
+# program transformation
+code = ["and", ["==", ["tmp"], ["next", ["orig"]]],
+               ["==", ["rev2"], ["orig"]],
+               ["==", ["orig2"], ["tmp"]]]
+
+# postcondition
+post = ["and", ["list2", ["orig2"]],
+               ["list2", ["rev2"]],
+               ["==", ["intersect", ["hlist2", ["orig2"]], ["hlist2", ["rev2"]]], ["\empty"]],
+               ["==", ["union", ["keys2", ["orig2"]], ["keys2", ["rev2"]]], ["$", "kk"]]]
+
+# axiom for next2
+axiom_next2 = ["==", ["$", "next2"],
+                     ["store", ["$", "next"], ["orig"], ["rev"]]]
+
+# frame rule
+frame_result = ["and", ["==", ["list", ["x"]], ["list2", ["x"]]],
+                       ["==", ["hlist", ["x"]], ["hlist2", ["x"]]],
+                       ["==", ["keys", ["x"]], ["keys2", ["x"]]]]
+frame_rule = ["implies", ["select", ["singleton", ["orig"]], ["x"]], frame_result]
+
+# collect terms and instantiate recursive defs and frame rule with those terms
+terms = collect_terms_formulas([pre, code, post, axiom_next2])
+formulas = remove_duplicates(instantiate(terms, ["x"], rec_defs + [frame_rule]))
+
+# repeat process using formulas from previous instantiation
+new_terms = collect_terms_formulas(formulas)
+print len(new_terms)
+
+new_formulas = remove_duplicates(instantiate(new_terms, ["x"], rec_defs + [frame_rule]))
+print len(new_formulas)
