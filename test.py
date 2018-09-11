53108fa12c55306e9e8b2e087d947feccb761656
Lucas Pena <lpena7@illinois.edu>
2018-09-11 11:06:13 -0500

    test: formatting, testing double instantiation and z3call

--- test.py
+++ test.py
@@ -32,13 +32,31 @@ def smt_print(formula):
     
     for terms in formula[i:]:
         out += smt_print(terms) + " "
-        
+
     return "(" + left + out + right + ")"
 
-#function to put VC together from elements and output SMT2 formula for Z3
+
+# function to put VC together from elements and output SMT2 formula for Z3
 def z3call(pre, code, post, inst):
     return "(and\n" + "(=>\n" + "   (and\n" + smt_print(pre) + "\n" + smt_print(code) + "\n   );pre and code\n" \
         + smt_print(post) + "\n);pre and code implies post\n ;these are inst\n" + smt_print(inst) \
         + ";end of inst" + "\n);end of formula"
-    
- 
+
+# Test
+def test():
+   listf = ["iff", ["list", ["x"]],
+                   ["or", ["==", ["x"], ["nil"]],
+                          ["and", ["!=", ["x"], ["nil"]],
+                                  ["list", ["next", ["x"]]]]]]
+
+   pre = ["True"]
+   code = ["==", ["a"], ["nil"]]
+   post = ["list", ["next", ["a"]]]
+
+   terms = collect_terms_formulas([pre, code, post])
+   inst = remove_duplicates(instantiate(terms, ["x", "y", "z"], [listf]))
+   # terms2 = collect_terms_formulas([pre, code, post] + inst)
+   # inst2 = remove_duplicates(instantiate(terms2, ["x", "y", "z"], [listf]))
+   return z3call(pre, code, post, inst[2])
+
+print test()
