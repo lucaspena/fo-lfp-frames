0e069b69539cc24ed6ae1d652d860ef094dadcad
Lucas Pena <lpena7@illinois.edu>
2020-02-07 15:26:34 -0600

    input-listlen: printing out definitions that can be parsed in CVC4

--- input-listlen.py
+++ input-listlen.py
@@ -1,4 +1,4 @@
-from z3_utils_hakank import *
+from z3 import *
 
 sol = Solver()
 
@@ -28,12 +28,94 @@ def vc(x, l, ret):
     return Implies( listlen(x, l),
                     Implies(pgm(x, l, ret), list(ret)))
 
-x1, l1, ret1 = Ints('x1 l1 ret1')
+# dereferenced terms in vc: { x }
 
+# TODO: enforce small false model?
+
+sol.add(next(-1) == -1)
 sol.add(ulist(-1))
-sol.add(listlen(x1, l1))
-sol.add(pgm(x1, l1, ret1))
-sol.add(Not(vc(x1, l1, ret1)))
+sol.add(ulistlen(-1, 0))
 
-print(sol.check())
-print(sol.model())
+x, l, xl, ret = Ints('x l xl ret')
+
+sol.add(listlen(x, l))
+sol.add(pgm(x, l, ret))
+
+# sol.add(ulist(ret))
+sol.add(ulist(x))
+sol.add(ulistlen(x, xl))
+
+sol.add(Implies(l == xl, ulistlen(x, l) == ulistlen(x, xl)))
+
+# sol.add(ulist(next(x)))
+# sol.add(ulist(next(ret)))
+
+sol.add(Not(vc(x, l, ret)))
+sol.check()
+
+m = sol.model()
+
+unary = { next : ['x Loc', 'Loc'],
+          list : ['x Loc', 'Bool']
+        }
+binary = { listlen : ['x Loc', 'y Loc', 'Bool'] }
+
+fcts = { next : ['x Loc', 'Loc'],
+         list : ['x Loc', 'Bool'],
+         listlen : ['x Loc', 'y Loc', 'Bool']
+       }
+
+defaults = { next : -1, list : False, listlen : False }
+
+# get header of function, i.e. (define-fun next ...
+def getHeader(elt):
+    header = '(define-fun ' + str(elt)
+    header += ' ('
+    length = len(fcts[elt])
+    for i in range(0, length-1):
+        header += '(' + fcts[elt][i] + ') '
+    header += ') '
+    header += fcts[elt][length-1]
+    return header
+
+# get body of unary function, ite expression
+def getUnaryBody(elt):
+    body = ''
+    length = len(fcts[elt])
+    for d in m.decls():
+        if not(d in fcts.keys()):
+            body += '  (ite '
+            body += '(= ' + fcts[elt][0].split(' ')[0] + ' ' + str(m[d]) + ') '
+            body += str(m.eval(elt(m[d]))).lower()
+            body += '\n'
+    return body
+
+# get body of binary function, ite expression
+def getBinaryBody(elt):
+    body = ''
+    length = len(fcts[elt])
+    for d1 in m.decls():
+        for d2 in m.decls():
+            if not(d1 in fcts.keys()) and not(d2 in fcts.keys()):
+                body += '  (ite (and '
+                body += '(= ' + fcts[elt][0].split(' ')[0] + ' ' + str(m[d1]) + ') '
+                body += '(= ' + fcts[elt][1].split(' ')[0] + ' ' + str(m[d2]) + ') '
+                body += str(m.eval(elt(m[d1], m[d2]))).lower()
+                body += '\n'
+    return body
+
+# print output in format CVC4 can handle
+for elt in fcts.keys():
+    print(getHeader(elt))
+    if elt in unary.keys():
+        print(getUnaryBody(elt), end = '')
+    elif elt in binary.keys():
+        print(getBinaryBody(elt), end = '')
+    print('  ', end = '')
+    for d in m.decls():
+        if not(d in fcts.keys()):
+            print(')', end = '')
+    print(' ' + str(defaults[elt]).lower())
+    print(')')
+    print()
+
